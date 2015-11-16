fc4f3b14edf9b178c74941ec92dce46f07c3b28a
abuicke <abuicke@treemetrics.com>
2015-11-16 19:58:12 +0000

    audio working

--- src/main/java/ie/headway/app/xml/StepUtils.java
+++ src/main/java/ie/headway/app/xml/StepUtils.java
@@ -12,14 +12,24 @@ public final class StepUtils {
   private StepUtils() {
   }
 
+  /**
+   * TODO: Quick impl, needs refactoring.
+   * */
   public static PortableStep contextualiseStep(final Step step, final Task task) {
     final File imgFile = new File(step.getImagePath());
-    final File newImg = new File(AppDir.ROOT.getPath(task.getName(), "imgs", task.getStepCount() + ".jpg"));
+    final File newImg = AppDir.ROOT.getFile(task.getName(), "imgs", task.getStepCount() + ".jpg");
+
+    final String audPath = step.getAudioPath();
+    final File newAud = !audPath.isEmpty() ? AppDir.ROOT.getFile(task.getName(), "auds", task.getStepCount() + ".3gp") : null;
 
     try {
       FileUtils.moveFile(imgFile, newImg);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+      if(newAud != null) {
+        FileUtils.moveFile(new File(audPath), newAud);
+        return new PortableStep(step.getText(), newImg.getAbsolutePath(), newAud.getAbsolutePath());
+      }
+    } catch (IOException ioe) {
+      throw new RuntimeException(ioe);
     }
 
     return new PortableStep(step.getText(), newImg.getAbsolutePath(), "");
